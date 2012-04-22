1736884c961ae7eb7361edf2e080c96c743f386c
Rede Linux Megazord Dev <admin@linux.ime.usp.br>
2012-04-22 19:47:19 -0300

    Kerberos module now supports not having a password.

--- lib/kerbwrap.py
+++ lib/kerbwrap.py
@@ -8,8 +8,13 @@ if __name__ == "__main__":
 	print "Esse módulo não deve ser executado diretamente."
 	quit()
 
+from supermegazord.db import path
+
 ROOT_PRINC = "root/admin"
-ROOT_PASS  = open('/etc/supermegazord-kerberos.secret').read()
+try:
+	ROOT_PASS = open(path.MEGAZORD_DB + "secrets/kerberos").read()
+except IOError:
+	ROOT_PASS = ""
 
 import os
 
