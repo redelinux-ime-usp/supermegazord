db23d797d2dfb41543faf25998a1671d2c6545a1
Rede Linux Megazord Dev <admin@linux.ime.usp.br>
2012-06-23 00:40:45 -0300

    Lets be awesome.

--- client/supermegazord.sh.py
+++ client/supermegazord.sh.py
@@ -12,24 +12,19 @@ if __name__ != "__main__":
 import sys, argparse
 
 if len(sys.argv) == 1:
-	print("Insert curses client here.")
+	import supermegazord.client.curse.curse_client as cursescli
+	cursescli.Run()
 	quit(0)
 
-def users_parser(args):
-	print "USER!"
-
 parser = argparse.ArgumentParser(description='Super Megazord.')
+
 subparsers = parser.add_subparsers(help='Possíveis scripts')
 
 import machine_parser
 machine_parser.prepare_parser(subparsers.add_parser('machines'))
 
-
-user_parse = subparsers.add_parser('users')
-user_parse.set_defaults(func=users_parser)
+import users_parser
+users_parser.prepare_parser(subparsers.add_parser('users'))
 
 args = parser.parse_args()
 args.func(args)
-
-print args
-
---
+++ client/users_parser.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python2.7
+# -*- coding: utf-8 -*-
+
+# Machine parser: Wrapper para o db.machines para linha de comando. 
+# Autor: Henrique Gemignani Passos Lima (henriquelima)
+# Escrito em: 2012-06-22
+
+if __name__ == "__main__":
+	print "Esse arquivo é um módulo."
+	quit()
+
+def prepare_parser(user_parse):
+	def users_parser(args):
+		print "hai"
+
+	import argparse
+	user_parse.set_defaults(func=users_parser)
+
