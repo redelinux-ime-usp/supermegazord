648ba4ac900b356375669ffd94d615cb6390cd3c
Rede Linux Megazord Dev <admin@linux.ime.usp.br>
2012-06-23 18:13:31 -0300

    Debug mode and validate permission.

--- system/remote_client.py
+++ system/remote_client.py
@@ -9,25 +9,33 @@ HOSTNAME = '' # Symbolic name meaning all available interfaces
 PORT = 15000
 BUFSIZ = 1024
 
+valid_commands = [ 'machines' ]
+
 #This function takes Bash commands and returns them
 def runMegazord(cmd):
-    p = subprocess.Popen("/opt/supermegazord/client/supermegazord.sh.py " + cmd, shell=True, stdout=subprocess.PIPE, stderr=subproccess.PIPE)
-    out = p.stdout.read().strip() + p.stderr.read().strip()
-    return out  #This is the stdout+stderr from the shell command
+	sp = cmd.split(' ')
+	if sp[0] not in valid_commands:
+		return "Permission denied."
+	p = subprocess.Popen("/opt/supermegazord/client/supermegazord.sh.py " + cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+	out = p.stdout.read().strip() + p.stderr.read().strip()
+	return out  #This is the stdout+stderr from the shell command
 
 ADDR = (HOSTNAME, PORT)
 
 #Function to control option parsing in Python
 def controller():
 	serversock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	fail = False
 	while True:
 		try:
 			serversock.bind(ADDR)
 			break
 		except:
+			fail = True
 			print "Error: failed to bind to port %s, trying again in 10 seconds." % PORT
 			import time
 			time.sleep(10)
+	if fail: print "Success."
 	serversock.listen(2)
 	while True:
 		clientsock, addr = serversock.accept()
@@ -41,6 +49,10 @@ def controller():
 		else:
 			clientsock.send("Invalid command: '" + data + "'")
 		clientsock.close()
-		
-with daemon.DaemonContext():
+	
+import sys
+if len(sys.argv) > 1 and sys.argv[1] == "-d":
 	controller()
+else:
+	with daemon.DaemonContext():
+		controller()
