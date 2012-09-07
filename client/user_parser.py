79706ead2d7292a3cce0018ce1c6a857bc6bb9d6
Dhalsim Root <admin@linux.ime.usp.br>
2012-09-07 13:44:15 -0300

    Refactors

--- client/user_parser.py
+++ client/user_parser.py
@@ -9,47 +9,54 @@ if __name__ == "__main__":
 	print "Esse arquivo é um módulo."
 	quit()
 
+import supermegazord.db.users as userdata
+
+def validate_type(atype, val):
+	if atype == 'auto':
+		if userdata.valida_nid(val):
+			atype = 'nid'
+		elif userdata.valida_login(val):
+			atype = 'login'
+		else:
+			atype = 'name'
+	return atype
+	
+def get_data(atype, u):
+	login = nid = curso = ingresso = nome = "n/a"
+
+	jupinfo = None
+	if atype == 'nid':
+		jupinfo = userdata.get_jupinfo_from_nid(u)
+		nid = u
+	elif atype == 'login':
+		jupinfo = userdata.get_jupinfo_from_login(u)
+		login = u
+	elif atype == 'name':
+		pass
+
+	if jupinfo:
+		nid = jupinfo.nid
+		curso = jupinfo.curso
+		ingresso = jupinfo.ingresso
+		nome = jupinfo.nome
+
+	return login, nid, curso, ingresso, nome
+
 def prepare_parser(user_parse):
-	import supermegazord.db.users as userdata
+
 	def user_parser(args):
-		print "    Login    |   NID  |Curso| Ingresso |  Nome" 
+		print "   Login    |   NID   | Curso | Ingresso |   Nome" 
 		for u in args.user:
-			atype = args.type	
-			if atype == 'auto':
-				if userdata.valida_nid(u):
-					atype = 'nid'
-				elif userdata.valida_login(u):
-					atype = 'login'
-				else:
-					atype = 'name'
-			
-			nid = ""
-			login = ""
-			nome = ""
-			jupinfo = None
-			if atype == 'nid':
-				jupinfo = userdata.get_jupinfo_from_nid(u)
-				nid = u
-			elif atype == 'login':
-				jupinfo = userdata.get_jupinfo_from_login(u)
-				login = u
-
-			elif atype == 'name':
-				#TODO
-				print "Suporte a procurar por nome não implementado"
-				continue
-
-			if jupinfo:
-				if nid == "": nid = jupinfo.nid
-				curso = jupinfo.curso
-				ingresso = jupinfo.ingresso
-				if nome == "": nome = jupinfo.nome
+			atype = validate_type(args.type, u)
+			login, nid, curso, ingresso, nome = get_data(atype, u)
 
 			# Trata tamanhos
+			login = (str(login) + " "*12)[:12]
 			nid = (" "*8 + str(nid))[-8:]
-			curso = (str(curso) + " "*5)[:5]
+			curso = (str(curso) + " "*6)[:6]
+			ingresso = (str(ingresso) + " "*10)[:10]
 
-			print " "*12, "|" + nid + "|" + curso + "|" + ingresso + "|" + nome
+			print login + "|" + nid + " | " + curso + "|" + ingresso + "| " + nome
 
 	import argparse
 	user_parse.add_argument('--type', choices=['nid', 'login', 'name', 'auto'], dest='type', default='auto')
