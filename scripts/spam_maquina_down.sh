7a10f9a79ebb815d0d5555f1757a46a9143c6cbd
Dhalsim Root <admin@linux.ime.usp.br>
2012-11-29 11:20:49 -0200

    Lets spam batata

---
+++ scripts/spam_maquina_down.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+DOWN=$(/opt/bin/watchman --down clients)
+LISTA=batata
+
+if [ "$DOWN" != "" ]; then
+	telnet mail 25 <<EOF
+helo linux.ime.usp.br
+mail from: $LISTA@linux.ime.usp.br
+rcpt to: $LISTA@linux.ime.usp.br
+data
+Content-Type: text/html; charset=UTF-8
+From: Megazord <megazord@linux.ime.usp.br>
+To: $LISTA@linux.ime.usp.br
+Subject: Máquinas Desligadas
+$DOWN
+
+.
+
+EOF
+
+fi
