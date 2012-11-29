e27082c0cdce9e4aee7741474a0a3f83aaa56f67
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2012-11-29 17:32:15 -0200

    Refactors

--- scripts/usuario/desativa_usuario.py
+++ scripts/usuario/desativa_usuario.py
@@ -7,43 +7,38 @@
 
 from supermegazord.db import users
 from supermegazord.db import path
-from supermegazord.lib import ldapwrap
-from supermegazord.lib.account import Account
+from supermegazord.lib import remote
+import supermegazord.lib.account as account
 
 import sys
 if len(sys.argv) != 2:
 	print "Uso: %s login" % sys.argv[0]
 	exit(1)
 
-login = sys.argv[1]
-dados = ldapwrap.find_user_by_login(login)
+user = account.from_login(sys.argv[1])
 
-if not dados:
+if not user:
 	print "Usuário inválido."
 	exit(1)
 
-group = ldapwrap.find_group_by_gid(dados['gidNumber'][0])['cn'][0]
+def comando(user):
+	return "sudo /megazord/scripts/desativa_conta " + user.login + " " + user.group.name
+	
+to_run = ['mail', 'printer', 'home', 'ldap']
+commands = {
+	'mail':    lambda user: remote.run_script("mail",    comando(user), "megazord") == 0,
+	'printer': lambda user: remote.run_script("printer", comando(user), "megazord") == 0,
+	'home':    lambda user: remote.run_script("home",    comando(user), "megazord") == 0,
+	'ldap':    lambda user: False
+}
 
-from supermegazord.lib import remote
 status_conta = {}
-
-comando = "sudo /megazord/scripts/desativa_conta " + login + " " + group
-
-print "Desativando do mail..."
-status_conta['mail']    = remote.run_script("mail",    comando, "megazord") == 0
-
-print "\nDesativando do printer..."
-status_conta['printer'] = remote.run_script("printer", comando, "megazord") == 0
-
-print "\nDesativando do NFS..."
-status_conta['home']    = remote.run_script("nfs",     comando, "megazord") == 0
-
-print "\nDesativando do LDAP..."
-print "Não implementado...\n"
-status_conta['ldap'] = False
+for running in to_run:
+	print "\nDesativando do {0}...".format(running)
+	status_conta[running] = commands[running](user)
 
 log = open(path.MEGAZORD_DB + "log/desativar", "a")
-log.write("Desativando usuário '{0}'; Status: {1}\n".format(login, str(status_conta)))
+log.write("Desativando usuário '{0}'; Status: {1}\n".format(user.login, str(status_conta)))
 
 for k in status_conta:
 	if not status_conta[k]:
