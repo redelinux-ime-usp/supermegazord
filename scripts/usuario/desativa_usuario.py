75ac4001438b91e92fec2b314d9deea271af0e19
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2012-12-03 18:46:19 -0200

    Desativa conta done.

--- scripts/usuario/desativa_usuario.py
+++ scripts/usuario/desativa_usuario.py
@@ -18,29 +18,35 @@ if len(sys.argv) != 2:
 user = account.from_login(sys.argv[1])
 
 if not user:
-	print "Usuário inválido."
+	print("Usuário inválido.")
 	exit(1)
 
 def comando(user):
 	return "sudo /megazord/scripts/desativa_conta " + user.login + " " + user.group.name
+	
+def ldap_disable(user):
+	group = user.group
+	return user.change_group('exalunos') and group.add_member(user)
 	
 to_run = ['mail', 'printer', 'home', 'ldap']
 commands = {
 	'mail':    lambda user: remote.run_script("mail",    comando(user), "megazord") == 0,
 	'printer': lambda user: remote.run_script("printer", comando(user), "megazord") == 0,
 	'home':    lambda user: remote.run_script("home",    comando(user), "megazord") == 0,
-	'ldap':    lambda user: False
+	'ldap':    lambda user: ldap_disable(user)
 }
 
 status_conta = {}
 for running in to_run:
-	print "\nDesativando do {0}...".format(running)
+	print("\nDesativando do {0}...".format(running))
 	status_conta[running] = commands[running](user)
 
+print
+	
 log = open(path.MEGAZORD_DB + "log/desativar", "a")
 log.write("Desativando usuário '{0}'; Status: {1}\n".format(user.login, str(status_conta)))
 
 for k in status_conta:
 	if not status_conta[k]:
-		print "Erro: etapa '" + k + "' mal-sucedida"
+		print("Erro: etapa '" + k + "' mal-sucedida")
 
