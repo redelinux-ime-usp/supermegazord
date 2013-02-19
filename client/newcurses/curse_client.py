0b8b7d3bfbb4eb9e81a2fe15d89a938f864cdc28
Dhalsim Root <admin@linux.ime.usp.br>
2013-02-19 01:20:59 -0300

    Ta awesum

--- client/newcurses/curse_client.py
+++ client/newcurses/curse_client.py
@@ -9,131 +9,161 @@
 # Modificado em: 2011-08-05 por henriquelima
 
 # Megazord global variables
-from supermegazord.system.megazord import Megazord
 import functools
 
+# Variaveis globais importantes
 quit = False
-
-# Memoriza a linha atual de cada menu
-current_line = 0
-
-# Lista atual de usuarios
-user_list = list()
+current_screen = None
+max_width = 80
 
 import curses, curses.textpad
-# Curses global variables
-curses_quit = False
-from supermegazord.base import colors
+import supermegazord.base.colors as colors
 
-def PrintUsers(screen, start, count, offset_y, offset_x):
-    start = max(start, 0)
-    for i in range(start, min(start+count, len(user_list))):
-        if i == current_line:
-            screen.addstr(offset_y + i - start, offset_x, user_list[i].login, colors.YELLOW)
-        else:
-            screen.addstr(offset_y + i - start, offset_x, user_list[i].login)
+def fill_with_spaces(s, size, right_side = True):
+    if right_side:
+        return (s + " " * size)[:size]
+    else:
+        return (" " * size + s)[-size:]
 
-def DrawBorders(screen, offset_y, offset_x, h, w, title = ""):
-    BORDER_TOP          = u'═'.encode('UTF-8')
-    BORDER_BOTTOM       = u'═'.encode('UTF-8')
-    BORDER_LEFT         = u'║'.encode('UTF-8')
-    BORDER_RIGHT        = u'║'.encode('UTF-8')
-    BORDER_TOP_LEFT     = u'╔'.encode('UTF-8')
-    BORDER_TOP_RIGHT    = u'╗'.encode('UTF-8')
-    BORDER_BOTTOM_LEFT  = u'╚'.encode('UTF-8')
-    BORDER_BOTTOM_RIGHT = u'╝'.encode('UTF-8')
-    try:
-        for x in range(1, w - 1):
-            screen.addstr(offset_y +    0 , offset_x + x, BORDER_TOP)
-            screen.addstr(offset_y + (h-1), offset_x + x, BORDER_BOTTOM)
-            for y in range(1, h-1):
-                screen.addstr(offset_y + y, offset_x + x, ' ')
-        for y in range(1, h-1):
-            screen.addstr(offset_y + y, offset_x +   0  , BORDER_LEFT)
-            screen.addstr(offset_y + y, offset_x + (w-1), BORDER_RIGHT)
-        screen.addstr(offset_y +   0  , offset_x +   0  , BORDER_TOP_LEFT)
-        screen.addstr(offset_y +   0  , offset_x + (w-1), BORDER_TOP_RIGHT)
-        screen.addstr(offset_y + (h-1), offset_x +   0  , BORDER_BOTTOM_LEFT)
-        screen.addstr(offset_y + (h-1), offset_x + (w-1), BORDER_BOTTOM_RIGHT)
-        if title != "":
-            screen.addstr(offset_y +   0  , offset_x +   4  , title)
-    except curses.error:
+def megazord_header(screen, section):
+    screen.addnstr(" " * 10 + "SUPERMEGAZORD - " + section + " " * max_width, max_width)
+
+class UserListScreen:
+    def __init__(self):
+        self.current_line = 0
+        self.page_size = 20
+        self.filtering = False
+        self.filter_string = ""
+        self.update_filter()
         pass
 
-def PrintMenu(menu, screen, offset_y = 0, offset_x = 0):
-    w = 40
-    h = len(menu.content) + 2
-    color = 2
-    
-    DrawBorders(screen, offset_y, offset_x, h, w, menu.name)
+    def change_line(self, newline):
+        self.current_line = max(0, min(newline, len(self.users) - 1))
 
-    global megazord, current_line
-    for i in range(0, len(menu.content)):
-        # Santas variáveis globais batman
-        if current_line[menu] == i:# and menu == megazord.active_menu:
-            screen.addstr(offset_y + i + 1, offset_x + 2, menu.content[i].name, colors.YELLOW)
+    def update_filter(self):
+        import supermegazord.lib.account as account
+        self.users = sorted(account.search(self.filter_string), key=lambda item: item.login)
+        self.change_line(self.current_line)
+
+    def filter_addchar(self, c):
+        if c == 27:
+            self.filtering = False
+            self.filter_string = ""
+        elif c == ord('\n'):
+            self.filtering = False
+        elif c >= ord('a') and c <= ord('z'):
+            self.filter_string += chr(c)
+        elif c == curses.KEY_BACKSPACE:
+            if len(self.filter_string) > 0:
+                self.filter_string = self.filter_string[:-1]
+            else:
+                self.filtering = False
         else:
-            screen.addstr(offset_y + i + 1, offset_x + 2, menu.content[i].name)
-        
-def PrintMenuList(screen):
-    global megazord
-    menu_list = [megazord.active_menu]
-    for menu in megazord.menu_history:
-        menu_list.append(menu)
-    
-    num_menu = len(menu_list)
-    for i in range(num_menu - 1, -1, -1):
-        PrintMenu(menu_list[i], screen, num_menu - i, num_menu - i)
+            return False
+        self.update_filter()
+        return True
 
-def GetInputSingle(win, i, script_arg):
-    if script_arg.default != "":
-        default_value = "[" + script_arg.default + "] "
-    else:
-        default_value = ""
+    def update(self, c):
+        global current_screen
+        if self.filtering:
+            return self.filter_addchar(c)
+        if c == ord('q'):
+            current_screen = None
+        elif c == ord('/'):
+            self.filtering = True
+            return True
+        elif c == curses.KEY_DOWN or c == ord('2'):
+            self.change_line(self.current_line + 1)
+            return True
+        elif c == curses.KEY_UP or c == ord('1'):
+            self.change_line(self.current_line - 1)
+            return True
+        elif c == curses.KEY_RIGHT or c == curses.KEY_NPAGE: # Page Down
+            self.change_line(self.current_line + self.page_size)
+            return True
+        elif c == curses.KEY_LEFT or c == curses.KEY_PPAGE: # Page Up
+            self.change_line(self.current_line - self.page_size)
+            return True
+        elif c == curses.KEY_END:
+            return True
+        elif c == curses.KEY_HOME:
+            return True
+        elif c == ord('\n'):
+            current_screen = userinfo_screen
+            userinfo_screen.current_user = self.users[self.current_line]
+            return True
+        return False
 
-    win.addstr(1 + i*2, 3, script_arg.description + " " + default_value)
-    win.refresh()
-    return script_arg.Parse(win.getstr(2 + i*2, 2, 40))
-    
+    def draw(self, screen):
+        current_page = int(self.current_line / self.page_size)
+        num_pages = int(len(self.users) / self.page_size)
+        start, count, offset_y, offset_x = current_page * self.page_size, self.page_size, 2, 0
+        start = max(start, 0)
+        megazord_header(screen, "Lista de Usuários")
+        screen.addnstr("\n" + fill_with_spaces("Login", 20+2) + fill_with_spaces("Grupo", 7), max_width)
+        for i in range(start, min(start+count, len(self.users))):
+            screen.addnstr(offset_y + (i % self.page_size), offset_x,
+                fill_with_spaces(self.users[i].login, 20) + "  " + fill_with_spaces(self.users[i].group.name, 7),
+                max_width, colors.YELLOW if i == self.current_line else colors.WHITE)
+        screen.addnstr(offset_y + count, offset_x, " " * 15 + "Page " + str(current_page + 1) + "/" + str(num_pages + 1), max_width)
+        if self.filtering:
+           screen.addnstr(offset_y + count + 1, offset_x, "/" + self.filter_string, max_width)
+
+class UserInfoScreen:
+    def __init__(self):
+        self.current_user = None
+
+    def update(self, c):
+        global current_screen
+        if c == ord('q'):
+            current_screen = None
+        elif c == 27:
+            current_screen = userlist_screen
+            return True
+        elif c >= ord('a') and c <= ord('z'):
+            print chr(c)
+        return False
+
+    def draw(self, screen):
+        import supermegazord.lib.jupinfo as libjupinfo
+        jupinfo = libjupinfo.from_nid(self.current_user.nid)
+        megazord_header(screen, "Informações de Usuário")
+        screen.addstr("\n")
+        screen.addnstr("\nLogin:    " + self.current_user.login, max_width)
+        screen.addnstr("\nNID:      " + self.current_user.nid, max_width)
+        screen.addnstr("\nCurso:    " + self.current_user.group.name, max_width)
+        screen.addnstr("\nIngresso: " + jupinfo.ingresso, max_width)
+        screen.addnstr("\nNome:     " + self.current_user.name, max_width)
+
+#=======================
+# Lista de telas
+userlist_screen = None
+userinfo_screen = None
+
 def main(screen):
     #screen.nodelay(True)
     curses.curs_set(0)
+    screen.timeout(1)
+    screen.notimeout(0)
 
-    global quit, current_line
     colors.init()
 
     redraw = True
-    while not quit:
+    while current_screen:
         if redraw:
             screen.clear()
-            PrintUsers(screen, current_line - 10, 20, 1, 1)
-            redraw = False
-        c = screen.getch()
-        if c == ord('q'):
-            quit = True
-        elif c == ord('/'):
-            pass
-        elif c == curses.KEY_DOWN or c == ord('2'):
-            current_line = current_line + 1
-            redraw = True
-        elif c == curses.KEY_UP or c == ord('1'):
-            current_line = current_line - 1
-            redraw = True
-        elif c == curses.KEY_LEFT:
-            redraw = True
-        elif c == curses.KEY_END:
-            redraw = True
-        elif c == curses.KEY_HOME:
-            redraw = True
-        elif c == ord('\n'):
-            pass
-        else:
-            screen.refresh()
+            current_screen.draw(screen)
+        try:
+            c = screen.getch()
+        except KeyboardInterrupt:
+            break
+        redraw = current_screen.update(c)
+        screen.refresh()
 
 def Run():
-    import supermegazord.lib.account as account
-    global user_list
-    user_list = sorted(account.search(""), key=lambda item: item.login)
+    global current_screen, userlist_screen, userinfo_screen
+    current_screen = userlist_screen = UserListScreen()
+    userinfo_screen = UserInfoScreen()
     curses.wrapper(main)
 
 if __name__ == "__main__":
