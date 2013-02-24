d478091d810dac5476a56d1ae41a2aa7ae640f18
Dhalsim Root <admin@linux.ime.usp.br>
2013-02-24 15:36:53 -0300

    lib.jupinfo native implementation

--- lib/jupinfo.py
+++ lib/jupinfo.py
@@ -6,24 +6,41 @@
 # Escrito em: 2012-02-29
 # Modificado em: 2012-01-13 por henriquelima
 
-class JupInfo:
-	def __init__(self, nid, nome, curso, ingresso):
-		self.nid = nid
-		self.nome = nome
-		self.curso = curso
-		self.ingresso = ingresso
+import collections
+class JupInfo(collections.namedtuple("JupInfo", "nid, nome, curso, ingresso")):
+    def __str__(self):
+        return "JupInfo: NID " + (" "*8 + str(self.nid))[-8:] + "; " + (str(self.curso) + " "*4)[:4] + (
+            "; Ing ") + self.ingresso + "; Nome: " + self.nome
 
-	def __str__(self):
-		return "NID:" + (" "*8 + str(self.nid))[-8:] + "; Curso: " + (str(self.curso) + " "*4)[:4] + (
-			"; Ingresso: ") + self.ingresso + "; Nome: " + self.nome
 
-	def __repr__(self):
-		return "JupInfo('%s','%s','%s','%s')" % (self.nid, self.nome, self.curso, self.ingresso)
+cache = {}
+def _convert_row(row):
+	data = row.strip().split(':')
+	try:
+		ingresso = data[3]
+	except:
+		ingresso = "n/a"
+	return JupInfo(data[0], data[1], data[2], ingresso)
 
+def _load_cache():
+	if len(cache) > 0: return
+	import supermegazord.db.path as path
+	for source in "jup_info", "nojup_info":
+		with open(path.MEGAZORD_DB + "/usuarios/jupiter/" + source) as f:
+			for line in f:
+				info = _convert_row(line)
+				cache[info.nid] = info
+	
 def from_nid(nid):
-	import supermegazord.db.users as users
-	return users.get_jupinfo_from_nid(nid)
+	_load_cache()
+	try:
+		return cache[nid]
+	except KeyError:
+		return None
 
 def from_login(login):
-	import supermegazord.db.users as users
-	return users.get_jupinfo_from_login(login)
+	import supermegazord.lib.account as account
+	acc = account.from_login(login)
+	if acc and acc.nid:
+		return from_nid(acc.nid)
+	return None
