ac4407955b1d56153e10cc7b68ddfbb02062206c
Dhalsim Root <admin@linux.ime.usp.br>
2013-03-06 11:36:47 -0300

    Descriptions.

--- client/watchman_parser.py
+++ client/watchman_parser.py
@@ -6,7 +6,8 @@
 # Escrito em: 2013-01-19
 
 def prepare_parser(megazordparser):
-	watch_parse = megazordparser.add_parser("watchman",	help="Check the status of the system's machines.")
+	watch_parse = megazordparser.add_parser("watchman",	
+					help="Check the status of the system's machines.")
 	setup_parser(watch_parse)
 
 def setup_parser(watch_parse):
@@ -30,7 +31,8 @@ def setup_parser(watch_parse):
 						print user,
 				print
 
-	watch_parse.description="List the system's machines according to their reachable status and more."
+	watch_parse.description = "List the system's machines according to their " + (
+		"reachable status and more.")
 	check_arg = watch_parse.add_mutually_exclusive_group(required=False)
 	check_arg.add_argument('--up'  , '-u', action='store_const', dest='checkfor', 
 		const=1, help="Report reachable machines.")
@@ -45,6 +47,6 @@ def setup_parser(watch_parse):
 		const=1, help="List logged in users along with the machine name.")
 	stats_arg.set_defaults(stats=0)
 
-	watch_parse.add_argument('group', choices=machines.groups(), default='all', nargs='?', metavar='group', 
-							help="Machine to group to operate on. Defaults to 'all'.")
+	watch_parse.add_argument('group', choices=machines.groups(), default='all',
+		nargs='?', metavar='group', help="Machine to group to operate on. Defaults to 'all'.")
 	watch_parse.set_defaults(func=watchman_parser)
