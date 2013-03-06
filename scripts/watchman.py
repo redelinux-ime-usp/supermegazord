c44f70b65d469c159de662182081d436bbdfc247
Rede Linux Megazord Dev <admin@linux.ime.usp.br>
2013-03-06 17:06:05 -0300

    Better handling of values.

--- scripts/watchman.py
+++ scripts/watchman.py
@@ -12,17 +12,11 @@ locale.setlocale(locale.LC_ALL, '')
 
 max_height = 24
 max_width  = 80
-max_namesize = 12
+MAX_NAMESIZE = 12
 
 sections = []
 statuses = {}
 colors = None
-counts = {
-	'unknown': 0,
-	'up': 0,
-	'down': 0,
-	'busy': 0
-}
 
 def mark_redraw():
 	global redraw
@@ -52,29 +46,40 @@ class ColorHolder:
 		self.RED     =      curses.color_pair(5)
 		self.RED_ON_WHITE = curses.color_pair(6)
 
+def addstr_newline(screen, s):
+	y, _ = screen.getyx()
+	screen.addstr(s)
+	ny, _ = screen.getyx()
+	if y == ny: screen.addstr("\n")
+
 class Section(collections.namedtuple("Section", "name machines")):
+	def draw_header(self, screen, width):
+		header = (u"═" * width)
+		header = header[:3] + self.name.decode("UTF-8") + header[4:]
+		header = u"╔" + header[0:width] + u"╗"
+		addstr_newline(screen, header.encode("UTF-8"))
+
+	def draw_item(self, screen, item):
+		status = statuses[item.hostname]
+		screen.addstr(" " if status.usage_avaible else "?", colors.MAGENTA)
+		screen.addnstr(item.hostname, MAX_NAMESIZE, status_color(status))
+		rest = MAX_NAMESIZE - len(item.hostname)
+		if rest > 0:
+			screen.addstr(" " * rest)
+
 	def draw(self, screen):
-		num_per_line = (max_width - 2) / (max_namesize + 1)
+		num_per_line = (max_width - 2) / (MAX_NAMESIZE + 1)
 		num_lines    = 1 + (max(len(self.machines) - 1, 0)) / num_per_line
-		total_width = min(num_per_line, len(self.machines)) * (max_namesize + 1)
-		y, _ = screen.getyx()
-		screen.addstr("╔═══" + self.name + ("═" * (total_width - len(self.name) - 3)) + "╗")
-		ny, _ = screen.getyx()
-		if y == ny: screen.addstr("\n")
+		total_width = min(num_per_line, len(self.machines)) * (MAX_NAMESIZE + 1)
+		self.draw_header(screen, total_width)
 		for line in range(num_lines):
 			y, _ = screen.getyx()
 			screen.addstr("║")
-			count = 0
-			for i in range(line * num_per_line, min((line+1)*num_per_line, len(self.machines))):
-				status = statuses[self.machines[i].hostname]
-				screen.addstr(" " if status.usage_avaible else "?", colors.MAGENTA)
-				screen.addnstr(self.machines[i].hostname, max_namesize, status_color(status))
-				rest = max_namesize - len(self.machines[i].hostname)
-				if rest > 0:
-					screen.addstr(" " * rest)
-				count += 1
-			if count * (max_namesize + 1) < total_width:
-				screen.addstr(" " * (total_width - count * (max_namesize + 1)))
+			count = min((line+1)*num_per_line, len(self.machines)) - (line * num_per_line)
+			for i in range(line * num_per_line, line * num_per_line + count):
+				self.draw_item(screen, self.machines[i])
+			if count * (MAX_NAMESIZE + 1) < total_width:
+				screen.addstr(" " * (total_width - count * (MAX_NAMESIZE + 1)))
 			screen.addstr("║")
 			ny, _ = screen.getyx()
 			if y == ny: screen.addstr("\n")
@@ -94,11 +99,23 @@ redraw = True
 def draw(screen):
 	screen.clear()
 	for section in sections:
-		section.draw(screen)
-	screen.addstr("UNK:"   + fill_with_spaces(counts['unknown'], 2, False), colors.WHITE)
-	screen.addstr(" UP: "  + fill_with_spaces(counts['up']     , 2, False), colors.GREEN)
-	screen.addstr(" DOWN:" + fill_with_spaces(counts['down']   , 2, False), colors.RED)
-	screen.addstr(" BUSY:" + fill_with_spaces(counts['busy']   , 2, False), colors.YELLOW)
+		try:
+			section.draw(screen)
+		except curses.error:
+			return
+	try:
+		counts = {
+			'unknown': len(filter(lambda x: not x.network_known, statuses.values())),
+			'up': len(filter(lambda x: x.network_known and not x.down, statuses.values())),
+			'down': len(filter(lambda x: x.network_known and x.down, statuses.values())),
+			'busy': len(filter(lambda x: len(x.users) > 0, statuses.values()))
+		}
+		screen.addstr("UNK:"   + fill_with_spaces(counts['unknown'], 2, False), colors.WHITE)
+		screen.addstr(" UP: "  + fill_with_spaces(counts['up']     , 2, False), colors.GREEN)
+		screen.addstr(" DOWN:" + fill_with_spaces(counts['down']   , 2, False), colors.RED)
+		screen.addstr(" BUSY:" + fill_with_spaces(counts['busy']   , 2, False), colors.YELLOW)
+	except curses.error:
+		return
 
 # O wrapper impede que o terminal fique zuado caso de alguma merda no script
 current_update = None
@@ -122,22 +139,14 @@ def main(screen):
 	for section in sections:
 		for m in section.machines:
 			statuses[m.hostname] = machine.Status(m)
-			counts['unknown'] += 1
 
 	import supermegazord.lib.worker as worker
 	current_update = worker.Processor()
 	def network(status):
-		if status.network_known:
-			counts['down' if status.down else 'up'] -= 1
-		else:
-			counts['unknown'] -= 1
 		status.query_network()
-		counts['down' if status.down else 'up'] += 1
 		mark_redraw()
 	def usage(status):
-		counts['busy'] -= min(len(status.users), 1)
 		status.query_usage()
-		counts['busy'] += min(len(status.users), 1)
 		mark_redraw()
 	for status in statuses.values():
 		current_update.add_job((network, status))
