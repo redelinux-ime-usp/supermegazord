0b0ab350396e3d6f0f19e7780e80ea051c416936
Dhalsim Root <admin@linux.ime.usp.br>
2013-04-01 20:14:53 -0300

    Clean lib/machine

--- lib/machine.py
+++ lib/machine.py
@@ -17,59 +17,10 @@ class Machine:
 		self.mac = mac
 		self.parent = parent
 		self.aliases = []
-		self.user_list = []
-		self.ResetStatus()
 
 	def Draw(self):
 		self.parent.Draw()
 
-	def Color(self):
-		result = 0
-		if self.QueryFinished() == False:
-			result = 0
-		elif self.Power() == False:
-			result = 2
-		elif self.UserList() != []:
-			result = 3
-		else:
-			result = 1	
-		return self.parent.Colors(result)
-
-	# Getters
-	def Name(self):
-		return self.hostname
-
-	def QueryFinished(self):
-		return self.query_finished
-	
-	def Power(self):
-		return self.power
-
-	def StatsAvaiable(self):
-		return self.stats_avaiable
-
-	def UserList(self):
-		return self.user_list
-
-	# Setters
-	def FinishQuery(self):
-		self.query_finished = True
-
-	def SetPower(self, power):
-		self.power = power
-
-	def SetStatsAvaiability(self, status):
-		self.stats_avaiable = status
-
-	def SetUserList(self, users):
-		self.user_list = users
-
-	def ResetStatus(self):
-		self.query_finished = False
-		self.power = True
-		self.stats_avaiable = True
-		self.user_list = []
-	
 	def __repr__(self):
 		return "Machine[hostname={0}]".format(self.hostname)
 
